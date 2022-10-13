128ac37f44f0bca5b014bd5c54811aaad3cc045b
catMeooww <110690475+catMeooww@users.noreply.github.com>
2022-10-13 15:07:43 -0300

    update

--- main.js
+++ main.js
@@ -7,3 +7,10 @@ var a = 10;
 var b = 5;
 var add = a + b;
 console.log(add)
+function console_calculo(){
+    var number = Number(document.getElementById("n1").value) + Number(document.getElementById("n2").value);
+    console.log(number);
+};
+function console_text(){
+    console.log(String(document.getElementById("p1").value));
+};
