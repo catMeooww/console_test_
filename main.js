7707e7e4f6da5a3b4d84f862ab197c42c4d54907
catMeooww <110690475+catMeooww@users.noreply.github.com>
2022-10-13 13:26:58 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log("hello!");
+console.log("esse é um teste de console");
+var first = "Guilherme";
+var last = "Bueno";
+console.log(first.concat(last));
+var a = 10;
+var b = 5;
+var add = a + b;
+console.log(add)
